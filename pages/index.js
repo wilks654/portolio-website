df5346ee4272ee13432cedaa91f422663c91be78
Sam Wilkinson <samwilkinson.github@gmail.com>
2020-01-03 15:38:19 +0000

    added social logos

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,46 @@
 import Hamburger from '../components/Hamburger';
 
+
 let style = <style jsx>{`
         h3 {
           text-align : center;
           font-size : 4em;
         }
+
+        #images-container {
+          display : flex;
+          justify-content: center;
+        }
+
+        #images-container img {
+          width : 2.75em;
+          height : 2.5em;
+        }
+
+        #images-container img:hover {
+          cursor : pointer;
+        }
+
+        #content-container {
+          margin-top : 15%;
+          width : 30em;
+          margin-left : auto;
+          margin-right: auto;
+        }
+
         `} 
 </style>
 
 const Index = () => (
     <div>
       <Hamburger />
-      <div>
+      <div id = 'content-container'>
         <h3>Sam Wilkinson</h3>
+        <div id = 'images-container'>
+          <img src='/github.png' alt="Logo" />
+          <img src='/codepen.svg' alt="Logo" />
+          <img src='/l-in.png' alt="Logo" />
+        </div>
       </div>
       {style}
     </div>
