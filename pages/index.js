4fb8ecaf8344c483ac416a0547f30d58f73b8b93
Sam Wilkinson <samwilkinson.github@gmail.com>
2020-01-05 13:04:33 +0000

    homepage + overlay
    added a background image to homepage;
    
    added an overlay;
    
    todo, change above to components with default values, but passable props.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Hamburger from '../components/Hamburger';
-
+import Head from 'next/head';
 
 let style = <style jsx>{`
         h3 {
@@ -21,28 +21,89 @@ let style = <style jsx>{`
           cursor : pointer;
         }
 
+        #background-image {
+          position : absolute;
+          top: 0em;
+          left: 0em;
+          width : 100%;
+          height : 100%;
+          background : url('/stock-background.jpg');
+          filter : blur(1px);
+        }
+
         #content-container {
-          margin-top : 15%;
-          width : 30em;
-          margin-left : auto;
-          margin-right: auto;
+          position : absolute;
+          width : 100%;
+          height : 100%;
+          top : 0em;
+          left : 0em;
+          z-index : 2;
+        }
+
+        #overlay {
+          position : absolute;
+          width : 100%;
+          height : 100%;
+          background-color : #253c4d;
+          opacity : 0.8;
+        }
+
+        #content-container > * {
+          position : relative;
+          z-index : 2;
         }
 
         `} 
 </style>
 
 const Index = () => (
-    <div>
+    <div id = ''>
+      <Head>
+        <title>Sam Wilkinson - Portfolio</title>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet"></link>
+      </Head>  
+
       <Hamburger />
+      <div id = 'background-image' />
+      <div id = 'overlay' />
+        
       <div id = 'content-container'>
         <h3>Sam Wilkinson</h3>
+        <i></i>
         <div id = 'images-container'>
-          <img src='/github.png' alt="Logo" />
-          <img src='/codepen.svg' alt="Logo" />
-          <img src='/l-in.png' alt="Logo" />
+          <img src='/github.png' alt="github logo" />
+          <img src='/codepen.svg' alt="codepen logo" />
+          <img src='/l-in.png' alt="linked-in logo" />
         </div>
-      </div>
+      </div>  
+      
       {style}
+      <style jsx global> 
+      {`
+        html, body {
+          width : 100%;
+          height : 100%;
+          padding: none;
+          margin: 0;
+
+        }
+        
+        html {
+          font-family: 'Source Sans Pro', sans-serif;
+        }
+
+        body {
+          
+          #background: url(/stock-background.jpg) no-repeat center center scroll;
+          background-blend-mode: lighten;
+          position : relative;
+          
+        }
+
+        
+        `
+      }
+      </style>
     </div>
 );
   
